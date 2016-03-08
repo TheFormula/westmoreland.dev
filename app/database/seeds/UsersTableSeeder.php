18cc1d3f10592a90793e959671f4aad41d7ab87a
Cameron Holland <cameron@thenearsky.com>
2016-03-08 11:27:56 -0600

    added second default user

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -4,7 +4,8 @@ class UsersTableSeeder extends Seeder {
 
 	public function run()
 	{
-		User::create(['first_name' => $_ENV['USER_FIRST'], 'last_name' => $_ENV['USER_LAST'], 'email' => $_ENV['USER_EMAIL'], 'password' => Hash::make($_ENV['USER_PASS'])]);
+		User::create(['first_name' => $_ENV['USER1_FIRST'], 'last_name' => $_ENV['USER1_LAST'], 'email' => $_ENV['USER1_EMAIL'], 'password' => Hash::make($_ENV['USER1_PASS'])]);
+		User::create(['first_name' => $_ENV['USER2_FIRST'], 'last_name' => $_ENV['USER2_LAST'], 'email' => $_ENV['USER2_EMAIL'], 'password' => Hash::make($_ENV['USER2_PASS'])]);
 	}
 
 }
