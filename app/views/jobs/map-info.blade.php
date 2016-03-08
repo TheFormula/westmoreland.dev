b94417bfbc2803b4ab28fd553de4ab4bb5cf0d21
Cameron Holland <cameron@thenearsky.com>
2016-03-08 09:57:23 -0600

    changed filler info window with info window design

--- app/views/jobs/map-info.blade.php
+++ app/views/jobs/map-info.blade.php
@@ -1,11 +1,14 @@
-<div class="col-xs-12 text-center">
-    <h4 class="text-center">{{{ $job->project_name }}}</h4>
-    <h3>{{{ $job->address }}}</h3>
-    <div class="center-block">
-        <img src="{{{ $job->image }}}" alt="Listing Image" class="img-responsive/>
-    </div>
-</div>
-<div class="col-xs-12 text-center">
-    <p>{{{ $job->description }}}</p>
-    <p>{{{ $job->hashtag }}}</p>
+<div class="w-map-overlay">
+	<img src="{{{ $job->image }}}" alt="{{{ $job->project_name }}}">
+	<div class="w-map-overlay-header">
+		<h2>{{{ $job->project_name }}}</h2>
+		<p>{{{ $job->address }}}</p>
+	</div>
+	<div class="w-map-overlay-body">
+		<h3>Project Summary</h3>
+		<p>{{{ $job->description }}}</p>
+		<h3>Social Media</h3>
+		<p>1/2/2016 - Can't wait to see the new #safishmarket building when it is done. @wbuildllc is doing an AMAZING job! - <a href="#">@jeremykarney</a></p>
+		<p>1/2/2016 - The #safishmarket building project is right on schedule!  Check out the pictures here: twitpic.com/safishmarket  - <a href="#">@wbuildllc</a></p>
+	</div>
 </div>
